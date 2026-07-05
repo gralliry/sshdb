db4ac9db5b0c1fcc1142c952f026183b6857fc6c
aiccy <aiccyxixy@163.com>
2026-07-05 11:31:33 +0800

    Add show command to display key contents

---
+++ cmd/show.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func showFunc(_ *cobra.Command, args []string) error {
+	name := args[0]
+	rec, err := Get(name)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Private key:\n%s\n", string(rec.PrivateKey))
+	fmt.Printf("Public key:\n%s\n", string(rec.PublicKey))
+	return nil
+}
+
+var showCmd = &cobra.Command{
+	Use:     "show <name>",
+	Aliases: []string{"s"},
+	Short:   "Show key contents",
+	Long:    `Print the private and public key data for a given key.`,
+	Args:    cobra.ExactArgs(1),
+	RunE:    showFunc,
+}
+
+func init() {
+	rootCmd.AddCommand(showCmd)
+}
